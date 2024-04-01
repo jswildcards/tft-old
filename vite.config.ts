5de4480a7ddfc978ce40f37f48b00b684fc44abc
jswildcards <eggkuv35c@relay.firefox.com>
2024-04-01 19:43:17 +0800

    * change production url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [vue()],
-    base: mode === 'production' ? '/tft/' : '/',
+    base: mode === 'production' ? '/tft-old/' : '/',
   }
 })
